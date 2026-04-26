1803537933fd4c3ed7ba240671a8b446bc1c90d3
muhammad-ahmed-gh <muhammad.ahmed.connect@gmail.com>
2026-04-26 18:03:24 +0300

    fix: fix pi and e replacement in getResult

--- src/utils/calculator.ts
+++ src/utils/calculator.ts
@@ -3,15 +3,17 @@ import { Parser } from "expr-eval";
 export const getResult = function (expr: string) {
   if (expr === "") return "";
 
-  const tmp = expr
-    .replace("π", `${Math.PI}`)
-    .replace("e", `${Math.E}`)
-    .replace("⨯", "*");
+  const finalExpr = expr
+    .replace(/(^|[+\-*/(])π/gi, `$1${Math.PI}`) // replace if π follows ( + - * / or is in the beginning
+    .replace(/(^|[+\-*/(])e/gi, `$1${Math.E}`)
+    .replaceAll("⨯", "*");
+
+  if (/[πe]/i.test(finalExpr)) return "";
 
   try {
     const parser = new Parser();
-    return parser.evaluate(tmp).toString();
+    return parser.evaluate(finalExpr).toString();
   } catch {
     return "";
   }
-}
+};
