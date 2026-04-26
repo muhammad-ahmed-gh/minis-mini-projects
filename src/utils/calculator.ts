3db8df89a088c7248465d75a22fcc521acc93305
muhammad-ahmed-gh <muhammad.ahmed.connect@gmail.com>
2026-04-26 18:06:44 +0300

    fix: change order of input preprocessing stages

--- src/utils/calculator.ts
+++ src/utils/calculator.ts
@@ -4,9 +4,9 @@ export const getResult = function (expr: string) {
   if (expr === "") return "";
 
   const finalExpr = expr
+    .replaceAll("⨯", "*")
     .replace(/(^|[+\-*/(])π/gi, `$1${Math.PI}`) // replace if π follows ( + - * / or is in the beginning
-    .replace(/(^|[+\-*/(])e/gi, `$1${Math.E}`)
-    .replaceAll("⨯", "*");
+    .replace(/(^|[+\-*/(])e/gi, `$1${Math.E}`);
 
   if (/[πe]/i.test(finalExpr)) return "";
 
