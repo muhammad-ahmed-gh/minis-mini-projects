181ea2c8d7cfd4dee70ba65d154443d612d69070
muhammad-ahmed-gh <muhammad.ahmed.connect@gmail.com>
2026-04-28 23:09:44 +0300

    feat: implement toggle done and favorite in to-do list

--- src/components/todoList/Task.tsx
+++ src/components/todoList/Task.tsx
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import { useTasks } from "../../hooks/todoList/useTasks";
 
 type Props = {
   label: string;
@@ -7,10 +8,43 @@ type Props = {
 };
 
 export default function Task(props: Props) {
+  const tasksState = useTasks();
+
+  const toggleDone = () => {
+    tasksState.setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.label === props.label
+          ? {
+              label: props.label,
+              isDone: !props.isDone,
+              isFavorite: props.isFavorite,
+            }
+          : task,
+      ),
+    );
+  };
+
+  const toggleFavorite = () => {
+    tasksState.setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.label === props.label
+          ? {
+              label: props.label,
+              isDone: props.isDone,
+              isFavorite: !props.isFavorite,
+            }
+          : task,
+      ),
+    );
+  };
+
   return (
     <div className="flex justify-between items-center px-[20px] border-1 border-[#eee] rounded-[15px] h-[65px]">
       <div className="flex items-center gap-[20px]">
-        <div className="text-primary text-[22px] cursor-pointer">
+        <div
+          className="text-primary text-[22px] cursor-pointer"
+          onClick={toggleDone}
+        >
           <i
             className={clsx(
               props.isDone
@@ -21,7 +55,10 @@ export default function Task(props: Props) {
         </div>
         <span>{props.label}</span>
       </div>
-      <div className="text-text-color-light cursor-pointer">
+      <div
+        className="text-text-color-light cursor-pointer"
+        onClick={toggleFavorite}
+      >
         <i
           className={clsx(
             props.isFavorite ? "fa-solid fa-star" : "fa-regular fa-star",
